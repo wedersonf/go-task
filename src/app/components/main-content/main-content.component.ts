5be8ed5a76a7793d0b1dac432631b58c65c89866
Wederson Fagundes <wedymf@hotmail.com>
2026-09-14 00:32:10 -0300

    feat: task list

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { WelcomeSectionComponent } from '../welcome-section/welcome-section.component';
+import { TaskListSectionComponent } from '../task-list-section/task-list-section.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [WelcomeSectionComponent],
+  imports: [WelcomeSectionComponent, TaskListSectionComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.css',
 })
