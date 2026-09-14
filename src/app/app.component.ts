7885524b5442d7d4005df277f7a7fbaba98f64fa
Wederson Fagundes <wedymf@hotmail.com>
2026-09-14 01:06:28 -0300

    feat: add comments modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { TaskFormModalComponent } from './components/task-form-modal/task-form-modal.component';
+import { TaskCommentsModalComponent } from './components/task-comments-modal/task-comments-modal.component';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,7 @@ import { TaskFormModalComponent } from './components/task-form-modal/task-form-m
     HeaderComponent,
     MainContentComponent,
     TaskFormModalComponent,
+    TaskCommentsModalComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
