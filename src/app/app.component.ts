5abd3fc5b90a21151479f16d9232fcd0c75274e6
Wederson Fagundes <wedymf@hotmail.com>
2026-09-14 00:20:24 -0300

    feat: welcome component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { WelcomeSectionComponent } from '../welcome-section/welcome-section.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [],
+  imports: [WelcomeSectionComponent],
   templateUrl: './main-content.component.html',
-  styleUrl: './main-content.component.css'
+  styleUrl: './main-content.component.css',
 })
-export class MainContentComponent {
-
-}
+export class MainContentComponent {}
