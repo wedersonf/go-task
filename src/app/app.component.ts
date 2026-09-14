196aa028d3f4d054d237b912879adb5175d6d05a
Wederson Fagundes <wedymf@hotmail.com>
2026-09-14 00:53:00 -0300

    feat: add task form modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
+import { TaskFormModalComponent } from './components/task-form-modal/task-form-modal.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, MainContentComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    MainContentComponent,
+    TaskFormModalComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
