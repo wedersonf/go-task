f2f09a2a822da8647066a6d8cd5255e6b1d9d9a0
Wederson Fagundes <wedymf@gmail.com>
2026-09-13 22:52:53 -0300

    feat: add base components

---
+++ src/app/components/task-list-section/task-list-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-list-section',
+  imports: [],
+  templateUrl: './task-list-section.component.html',
+  styleUrl: './task-list-section.component.css'
+})
+export class TaskListSectionComponent {
+
+}
