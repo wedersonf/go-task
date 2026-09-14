31c16458ede90dbbdf35c350cfbb2a5ed462104e
Wederson Fagundes <wedymf@hotmail.com>
2026-09-14 00:39:09 -0300

    feat: add card component

--- src/app/components/task-list-section/task-list-section.component.ts
+++ src/app/components/task-list-section/task-list-section.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { TaskCardComponent } from '../task-card/task-card.component';
 
 @Component({
   selector: 'app-task-list-section',
-  imports: [],
+  imports: [TaskCardComponent],
   templateUrl: './task-list-section.component.html',
-  styleUrl: './task-list-section.component.css'
+  styleUrl: './task-list-section.component.css',
 })
-export class TaskListSectionComponent {
-
-}
+export class TaskListSectionComponent {}
