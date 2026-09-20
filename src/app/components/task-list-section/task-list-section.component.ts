a5f46cbf4ab0e8dd18c20a50bb842ebf8cbd9404
Wederson Fagundes <wedymf@hotmail.com>
2026-09-20 01:18:31 -0300

    feat: add drag drop

--- src/app/components/task-list-section/task-list-section.component.ts
+++ src/app/components/task-list-section/task-list-section.component.ts
@@ -1,21 +1,56 @@
 import { Component, inject } from '@angular/core';
 import { TaskCardComponent } from '../task-card/task-card.component';
 import { TaskService } from '../../services/task.service';
+import {
+  CdkDropList,
+  CdkDrag,
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+import { ITask } from '../../interfaces/task.interface';
 
 @Component({
   selector: 'app-task-list-section',
-  imports: [TaskCardComponent],
+  imports: [TaskCardComponent, CdkDropList, CdkDrag],
   templateUrl: './task-list-section.component.html',
   styleUrl: './task-list-section.component.css',
 })
 export class TaskListSectionComponent {
+  todoTasks: ITask[] = [];
+  doingTasks: ITask[] = [];
+  doneTasks: ITask[] = [];
+
   private readonly _taskService = inject(TaskService);
 
   ngOnInit() {
     this._taskService.todoTasks.subscribe((todoList) => {
-      console.log('Lista todo:', todoList);
-
-      todoList[0].name = 'Nome alterado';
+      this.todoTasks = todoList;
     });
+
+    this._taskService.doingTasks.subscribe((todoList) => {
+      this.doingTasks = todoList;
+    });
+
+    this._taskService.doneTasks.subscribe((todoList) => {
+      this.doneTasks = todoList;
+    });
+  }
+
+  drop(event: CdkDragDrop<ITask[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 }
