f854b72928a93c1bb1adcc3a2c14241711c8f672
Wederson Fagundes <wedymf@hotmail.com>
2026-09-20 00:30:21 -0300

    feat: feat get modal return

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -11,9 +11,13 @@ export class TaskCardComponent {
   private readonly _modalControllerService = inject(ModalControllerService);
 
   openEditTaskModal() {
-    this._modalControllerService.openEditTaskModal({
+    const dialogRef = this._modalControllerService.openEditTaskModal({
       name: 'Nova tarefa',
       description: 'Descrição tarefa',
+    });
+
+    dialogRef.closed.subscribe((taskForm) => {
+      console.log('Tarefa alterada:', taskForm);
     });
   }
 }
--- src/app/components/welcome-section/welcome-section.component.ts
+++ src/app/components/welcome-section/welcome-section.component.ts
@@ -11,6 +11,10 @@ export class WelcomeSectionComponent {
   private readonly _modalControllerService = inject(ModalControllerService);
 
   openCreateTaskModal() {
-    this._modalControllerService.openNewTaskModal();
+    const dialogRef = this._modalControllerService.openNewTaskModal();
+
+    dialogRef.closed.subscribe((taskForm) => {
+      console.log('Tarefa criada:', taskForm);
+    });
   }
 }
